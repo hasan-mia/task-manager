22d9e63a4ac4f1232fd9902021a76d24e4ae14cb
hasan-mia <hasanrafi69@gmail.com>
2022-09-13 17:56:03 +0600

    added header and footer

--- src/Components/Header/Menu.js
+++ src/Components/Header/Menu.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Menu = () => {
 	return (
-	<header className='px-1 lg:px-8 border-b'>
-		<div className="navbar bg-base-100">
-			<div className="flex-1"> <Link to="/" className="uppercase font-semibold border p-1 lg:border-0 text-xl">To-Do</Link> </div>
+	<header className='px-1 lg:px-8 border-b bg-gray-900'>
+		<div className="navbar">
+			<div className="flex-1"> <Link to="/" className="uppercase text-gray-50 font-semibold border p-1 lg:border-0 text-xl">Task-Manager</Link> </div>
 			<div className="flex-none">
-				<ul className="menu menu-horizontal p-0">
-					<li><Link to="/completed">Completed</Link></li>
-					<li><Link to="/todo">To-Do</Link></li>
-					<li><Link to="/calendar">Calendar</Link></li>
+				<ul className="menu menu-horizontal p-0 text-gray-50">
+					<li className='text-gray-50'><Link to="/">Home</Link></li>
+					<li className='text-gray-50'><Link to="/todo">Task List</Link></li>
+					<li className='text-gray-50'><Link to="/completed">Completed</Link></li>
+					{/* <li><Link to="/calendar">Calendar</Link></li> */}
 				</ul>
 			</div>
 		</div>
