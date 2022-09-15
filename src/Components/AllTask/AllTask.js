7253a5f495169875e051fb421c7e175c23b95d38
hasan-mia <hasanrafi69@gmail.com>
2022-09-15 15:18:00 +0600

    priority filtering aded but not implement

--- src/Components/AllTask/AllTask.js
+++ src/Components/AllTask/AllTask.js
@@ -1,10 +1,34 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { taskContext } from '../../App';
 
 const AllTask = () => {
+	// all tasks
 	const {tasks} = useContext(taskContext);
+	// data filter 
+	const[priorityBase, setPriorityBase]=useState(tasks?.tasks);
+	const [hight, setHigh] = useState([])
+	
+	// High Priority task
+	const highPriority = tasks?.tasks.find(element => {
+		return (element.priority == 3)
+	});
+	console.log(highPriority)
+
+	// Medium Priority task
+	const mediumPriority = tasks?.tasks.find(element => {
+		return (element.priority == 2)
+	});
+	console.log(mediumPriority)
+
+	// Low Priority task
+	const lowPriority = tasks?.tasks.find(element => {
+		return (element.priority == 1)
+	});
+	console.log(lowPriority)
+
+	// update route navigator
 	const updateTask = useNavigate();
 	const token = {
 		'AuthToken': 'UrM4YHgb1FcqEf1tuKwmAMMX5MxFZ12a'
@@ -29,10 +53,10 @@ const AllTask = () => {
 	}
 	return (
 		<div className='min-h-screen'>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-			{
-				tasks.tasks?.map( task => 
-					<div key={task.id} className="card w-full bg-base-100 shadow-xl">
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+				{
+					tasks.tasks?.map( task => 
+					<div key={task.id} className="card shadow-xl">
 						<div className="card-body">
 							<h2 className="card-title">{task?.assigned_name}</h2>
 							<p>{task?.message}</p>
@@ -42,9 +66,61 @@ const AllTask = () => {
 							</div>
 						</div>
 					</div>
-				)
-			}
-		</div>
+					)
+				}
+{/* 		
+				<div>
+				<button>High</button>
+					{
+					tasks.tasks?.map( task => 
+					<div key={task.id} className="card shadow-xl">
+						<div className="card-body">
+							<h2 className="card-title">{task?.assigned_name}</h2>
+							<p>{task?.message}</p>
+							<div className="card-actions justify-end">
+								<button onClick={()=>deleteTask(task.id)} className="btn btn-error text-gray-50">Delete</button>
+								<button onClick={()=>updateTask(`/update/${task.id}`)} className="btn btn-primary">Update</button>
+							</div>
+						</div>
+					</div>
+					)}
+				</div>
+
+				<div>
+				<button>Medium</button>
+					{
+					tasks.tasks?.map( task => 
+					<div key={task.id} className="card shadow-xl">
+						<div className="card-body">
+							<h2 className="card-title">{task?.assigned_name}</h2>
+							<p>{task?.message}</p>
+							<div className="card-actions justify-end">
+								<button onClick={()=>deleteTask(task.id)} className="btn btn-error text-gray-50">Delete</button>
+								<button onClick={()=>updateTask(`/update/${task.id}`)} className="btn btn-primary">Update</button>
+							</div>
+						</div>
+					</div>
+					)}
+				</div>
+
+				<div>
+				<button>Low</button>
+					{
+					tasks.tasks?.map( task => 
+					<div key={task.id} className="card shadow-xl">
+						<div className="card-body">
+							<h2 className="card-title">{task?.assigned_name}</h2>
+							<p>{task?.message}</p>
+							<div className="card-actions justify-end">
+								<button onClick={()=>deleteTask(task.id)} className="btn btn-error text-gray-50">Delete</button>
+								<button onClick={()=>updateTask(`/update/${task.id}`)} className="btn btn-primary">Update</button>
+							</div>
+						</div>
+					</div>
+					)}
+				</div>
+		 */}
+			</div>
 		</div>
 	);
 };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
 		<main className='px-4 lg:px-8 py-8 w-full min-h-screen'>
 			<div className='flex flex-wrap flex-row justify-center'>
 
-				<div className='flex-shrink w-full px-0 my-2 w-12/12 md:w-6/12 lg:w-4/12 xl:px-6 border lg:border-0'>
+				<div className='flex-shrink w-full px-0 my-2 w-12/12 md:w-6/12 lg:w-3/12 xl:px-2 border lg:border-0'>
 					
 					<div className='fixed mt-12'>
 						<h2 className='uppercase pb-2 text-center lg:text-left font-semibold text-xl'>ADD YOUR TASK</h2>
@@ -19,7 +19,7 @@ const Home = () => {
 					</div>
 				</div>
 
-				<div className='flex-shrink w-full px-0 my-2 w-12/12 md:w-6/12 lg:w-8/12 xl:px-6 border lg:border-0'>
+				<div className='flex-shrink w-full px-0 my-2 w-12/12 md:w-6/12 lg:w-9/12 xl:px-2 border lg:border-0'>
 					<div className="mt-12">
 					<h2 className='uppercase mt-6 lg:mt-0 pb-2 text-center lg:text-left font-semibold text-xl'>YOUR TASK LIST</h2>
 					{
